c7854ab2fa3bd13a5ac9ee198bd6c079d74f89cb
Ryan Harrington <rharrington@microsoft.com>
2024-10-07 10:23:18 -0400

    changes to multiple files

---
+++ scripts/metadata_scan.py
@@ -0,0 +1,22 @@
+from src.service_principal import ServicePrincipal
+from src.scan import Scan   
+import json
+
+
+
+# base tests
+config_data = json.loads(open('docs/non-prod-spn-config.json').read())
+spn = ServicePrincipal(
+    client_id=config_data['client_id'],
+    tenant_id=config_data['tenant_id'],
+    spn_secret_name=config_data['spn_secret_name'],
+    vault_url=config_data['vault_url']
+)
+    
+scan = Scan(spn=spn)
+
+# pass scan request id to get scan status
+scan_status = scan.get_scan_status(scan_id=scan.scan_request['id'])
+
+
+print(scan.scan_results)
